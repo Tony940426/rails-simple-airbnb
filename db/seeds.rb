96501a96095e9ff7e739bfd7961ec4b737185ca8
Tony Wang <tony94042@gmail.com>
2022-11-18 13:31:45 +1100

    Added model, basic styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Clearing Database'
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Generous Space Ground Floor Apartment Melbourne',
+  address: '64 Edgar St North Glen Iris',
+  description: 'Enjoy Breakfast on your private breakfast balcony',
+  price_per_night: 50,
+  number_of_guests: 2
+)
+
+
+puts 'Database reseeded'
