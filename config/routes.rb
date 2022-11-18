b804182118b027d757442b881d440ebdc0aa0315
Tony Wang <tony94042@gmail.com>
2022-11-18 14:38:46 +1100

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'flats', to: 'flats#index'
+  get 'flats/new', to: 'flats#new'
   get 'flats/:id', to: 'flats#show'
 end
